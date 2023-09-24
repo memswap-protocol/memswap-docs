cabb3f450881c53403c4c13044abac0cdb6309c9
Ted Palmer <etpalmer17@gmail.com>
2023-09-24 11:30:03 -0400

    Add prettier, remove vercel footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
-import type { AppProps } from "next/app";
-import { PostHogProvider } from "posthog-js/react";
-import posthog from "posthog-js";
+import type { AppProps } from 'next/app'
+import { PostHogProvider } from 'posthog-js/react'
+import posthog from 'posthog-js'
 
-if (typeof window !== "undefined") {
+if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: "https://app.posthog.com",
-  });
+    api_host: 'https://app.posthog.com',
+  })
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PostHogProvider client={posthog}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </PostHogProvider>
-  );
+  )
 }
